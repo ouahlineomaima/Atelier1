2b8edaebbc71a61759c575d17d7e7bbfe2d0d5da
omaima ouahline <ouahline.omaima@ine.inpt.ma>
2022-11-13 14:06:08 +0100

    test merge

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,9 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hi! I'm scrum master");
 		System.out.println("Hi scrum master! I'm dev");
+		System.out.println("test rebase");
 		System.out.println("test merge");
+		
 
 	}
 
